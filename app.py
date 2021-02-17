f80b75122576ee9400795f580eafaa1a9b362e41
Er Jun Li <er.li@mail.mcgill.ca>
2021-02-17 18:49:48 -0500

    Post route and query params route

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import os
+
 app = Flask(__name__)
+database = []
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/processPOSTData', methods=["POST"])
+def postdata():
+    # TODO: Handle the edge cases - Handle empty json, etc.  empty json 
+    data = request.get_json()
+    values = list(data.values())
+    return {"values": values}, 200
+
+@app.route('/processQueryData', methods=["GET"])
+def queryParams():
+    # TODO: Handle the edge cases.
+    return request.args.to_dict(), 200
 
 if __name__ == "__main__":
     os.environ['FLASK_ENV'] = 'development'
