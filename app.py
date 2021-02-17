c2212ceb52da6938fc29f9e3418bb35f19d3aed5
Er Jun Li <er.li@mail.mcgill.ca>
2021-02-17 18:21:26 -0500

    Running in dev

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,5 +7,5 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == "__main__":
-    os.environ["FLASK_ENVIRONMENT"] = "development"
+    os.environ['FLASK_ENV'] = 'development'
     app.run()
