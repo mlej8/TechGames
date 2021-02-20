79468b2105788469f64952129305e105781c1cb8
Li Xin Rui <li.xinrui3@gmail.com>
2021-02-20 14:10:53 -0500

    finished spirit growth

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
 import os
+import requests
 
 app = Flask(__name__)
 
 database = ["string1", "string2"]
+API = "a89ffb0315bbb15cf4401353f297a3d7"
 
 @app.route('/', methods=["GET"])
 def hello_world():
@@ -48,6 +50,27 @@ def postdata():
 def queryParams():
     # TODO: Handle the edge cases.
     return request.args.to_dict(), 200
+
+@app.route('/spiritgrowth', methods=["POST"])
+def returnplaylist():
+    data = request.get_json()
+    if data.get("city"):
+        res = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={data.get("city")}appid={API}')
+        print(res.json())
+        playlist = []
+        if res.json().get("main").get("temp"):
+            temp = res.json().get("main").get("temp")
+            if temp >= 30:
+                playlist.append("Party Rock - LMFAO")
+            elif temp >= 15 and temp < 30:
+                playlist.append("Tick Tock - Kesha")
+            elif temp >= 10 and temp < 15:
+                playlist.append("Purple Haze - Jimi Hendrix")
+            else:
+                playlist.append("Four Seasons - Vivaldi")
+        return jsonify(playlist), 201
+    else:
+        return {}, 400
 
 if __name__ == "__main__":
     os.environ['FLASK_ENV'] = 'development'
