2471f4cfc8813df3c9f15bae4579c7a7fc2d33e7
Li Xin Rui <li.xinrui3@gmail.com>
2021-02-20 13:04:38 -0500

    finished status, processPathData, data

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask
+from flask import jsonify
 import os
 app = Flask(__name__)
+
+database = ["string1", "string2"]
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/status')
+def get_func():
+    res = {
+        "status": "up"
+    }
+    return jsonify(res)
+
+@app.route('/processPathData/<pathParameter>')
+def echo_path(pathParameter):
+    res = {
+        "pathParam": pathParameter
+    }
+    return jsonify(res)
+
+@app.route('/data')
+def data():
+    return jsonify(database)
 
 if __name__ == "__main__":
     os.environ['FLASK_ENV'] = 'development'
